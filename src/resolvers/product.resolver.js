ad7d6e5246ed89041232a58ccc3b5ceb1d1d99e2
Fenil <fenil3357@gmail.com>
2024-05-29 10:50:05 +0530

    Adds: product resolver

---
+++ src/resolvers/product.resolver.js
@@ -0,0 +1,49 @@
+import { createProduct } from "../db/database_functions/product/createProduct.js";
+import { deleteProduct } from "../db/database_functions/product/deleteProduct.js";
+import { updateProduct } from "../db/database_functions/product/updateProduct.js";
+import { getProducts } from "../db/database_functions/product/getProducts.js";
+import { getProduct } from "../db/database_functions/product/getProduct.js";
+
+export const ProductResolvers = {
+  async getProduct({ _id }) {
+    try {
+      const product = await getProduct(_id);
+      return product;
+    } catch (error) {
+      throw error;
+    }
+  },
+  async getProducts() {
+    try {
+      const products = await getProducts();
+    } catch (error) {
+      throw error;
+    }
+  },
+  async createProduct({ name, price }) {
+    try {
+      const product = await createProduct({ name, price })
+      return product;
+    } catch (error) {
+      throw error;
+    }
+  },
+  async updateProduct({ _id, ...dataToUpdate }) {
+    try {
+      const product = await updateProduct(_id, dataToUpdate);
+      return product;
+    } catch (error) {
+      throw error;
+    }
+  },
+  async deleteProduct({ _id }) {
+    try {
+      await deleteProduct(_id);
+      return {
+        message: "Product deleted successfully!"
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+}
